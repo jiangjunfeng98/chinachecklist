e38bb0f6c176f07d33254df12b933d9e1db0284a
几个霜 <510879374@qq.com>
2024-10-26 22:39:56 +0800

    服务信息

---
+++ src/data/resport.ts
@@ -0,0 +1,63 @@
+// 基础报告
+export const baseReport = [
+  {
+    //工商信息
+    'title': 'Registration Details',
+    'list': [
+      // 企业名称
+      'Official Company Name',
+      // 法定代表人
+      'Legal Representative',
+      // 登记状态
+      'Registration Status',
+      // 成立日期
+      'Date of Establishment',
+      // 注册资本
+      'Registered Capital',
+      // 实缴资本
+      'Paid-up Capital',
+      // 工商注册号
+      'Registration Number',
+      // 企业类型
+      'Company Type',
+      // 营业期限
+      'License Expiry Date',
+      // 业务运营状况
+      'Business Operations Status',
+      // 注册地址
+      'Registered Address',
+      // 登记机关
+      'Registration Bureau',
+      // 纳税人识别号
+      'Tax ID Number',
+      // 参保人数
+      'Number of Insured Employees',
+      // 经营范围
+      'Business Scope'
+    ]
+  },
+  // 公司董事
+  {
+    'title': 'Company Directors',
+    'note': 'For Each Director',
+    'list': [
+      //董事姓名
+      'Director Name',
+      // 董事职衔
+      'Director Title'
+    ]
+  },
+  // 公司股东
+  {
+    'title': 'Company Shareholders',
+    'note': 'For Each Shareholder',
+    'list': [
+      //股东姓名
+      'Shareholder Name',
+      // 股东持股
+      'Shareholder Holding',
+      // 更新日期
+      'Date of Latest Investment'
+    ]
+  }
+];
