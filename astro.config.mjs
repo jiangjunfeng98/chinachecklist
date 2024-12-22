021b5326f01740149c687b0e2c357a439410ffa9
几个霜 <510879374@qq.com>
2024-12-22 21:33:49 +0800

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify()
+  adapter: cloudflare()
 });
