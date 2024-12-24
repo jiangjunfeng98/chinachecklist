301c74607ffcc2523a660fce0b9325e64b8f9178
几个霜 <510879374@qq.com>
2024-12-24 22:08:54 +0800

    更新api描述

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  // output: 'server',
+  output: 'server',
   adapter: cloudflare({
     platformProxy: {
       enabled: true
