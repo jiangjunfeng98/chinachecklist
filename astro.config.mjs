5b1366183ad51cc2210300c5b9bfcc6e8231b3f0
jjf <510879374@qq.com>
2024-12-29 21:38:25 +0800

    cf test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 import cloudflare from "@astrojs/cloudflare";
 
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
-  // adapter: vercel()
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
