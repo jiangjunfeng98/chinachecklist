5ee6e8b81ea3049c3c15bcc581d9d53053761165
jjf <510879374@qq.com>
2024-12-29 21:44:31 +0800

    cf test

--- src/pages/api/api-test.ts
+++ src/pages/api/api-test.ts
@@ -1,9 +1,11 @@
 // Outputs: /builtwith.json
 export async function GET({ params, request }) {
+  const time = new Date().toISOString();
   return new Response(
     JSON.stringify({
       name: 'Astro',
-      url: 'https://astro.build/'
+      url: 'https://astro.build/',
+      time: time
     })
   )
 }
